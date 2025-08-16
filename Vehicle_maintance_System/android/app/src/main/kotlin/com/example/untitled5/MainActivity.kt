67a1085302f26359c6ea4442852c08a84572e19e
Thushanthadasun <thushanthadasun25@gmail.com>
2025-08-16 12:00:26 +0530

    Update MainActivity.kt

--- Vehicle_maintance_System/android/app/src/main/kotlin/com/example/untitled5/MainActivity.kt
+++ Vehicle_maintance_System/android/app/src/main/kotlin/com/example/untitled5/MainActivity.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.example.vehicle_maintenance
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
-=======
-package com.example.untitled5
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 43376c16547ad2f3c7b6cbde5b95fad1c8c053d7
